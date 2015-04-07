00d3f20b97464d78b091d4ddd0fc2b09d69614f9
Everest Liu <Everest.FuboLiu@gmail.com>
2015-04-06 23:55:39 -0400

    Update LogEntry.java

--- src/LogEntry.java
+++ src/LogEntry.java
@@ -2,6 +2,7 @@ import java.util.ArrayList;
 
 public class LogEntry {
     private ArrayList<String> lines;
+    private int num_lines = 0;
     
     public LogEntry() {
         lines = new ArrayList<String>();
@@ -9,9 +10,20 @@ public class LogEntry {
     
     public void addLine(String line) {
         lines.add(line);
+        num_lines++;
     }
     
     public String getLine(int index) {
         return lines.get(i);
+    }
+    
+    public String toString() {
+        String str = "";
+        
+        for (int i = 0; i < num_lines; i++) {
+            str += num_lines.get(i) + "\n";
+        }
+        
+        return str;
     }
 }
