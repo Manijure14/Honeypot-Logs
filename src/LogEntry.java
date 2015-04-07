a04cf1fa8c69c09135cb569e4844cb337801b1a7
Everest Liu <Everest.FuboLiu@gmail.com>
2015-04-06 23:49:52 -0400

    Update LogEntry.java

--- src/LogEntry.java
+++ src/LogEntry.java
@@ -1,5 +1,17 @@
-
+import java.util.ArrayList;
 
 public class LogEntry {
-
+    private ArrayList<String> lines;
+    
+    public LogEntry() {
+        lines = new ArrayList<String>();
+    }
+    
+    public void addLine(String line) {
+        lines.add(line);
+    }
+    
+    public String getLine(int index) {
+        return lines.get(i);
+    }
 }
