605ba275b1a4078d4865fadc7f598a76d818f4da
Everest Liu <Everest.FuboLiu@gmail.com>
2015-04-07 15:57:41 -0400

    Update LogEntry.java

--- src/LogEntry.java
+++ src/LogEntry.java
@@ -2,6 +2,8 @@ import java.util.ArrayList;
 
 public class LogEntry {
     private ArrayList<String> lines;
+    private ArrayList<String> usernames;
+    private ArrayList<String> passwords;
     private int num_lines = 0;
     
     public LogEntry() {
@@ -11,6 +13,18 @@ public class LogEntry {
     public void addLine(String line) {
         lines.add(line);
         num_lines++;
+    }
+    
+    public void addUsername(String username) {
+        usernames.add(username);
+    }
+    
+    public void addPassword(String password) {
+        passwords.add(password);
+    }
+    
+    public String getUserPassword(int index) {
+        return usernames.get(index) + " " + passwords.get(index);
     }
     
     public String getLine(int index) {
